7ae1c1339dfa2f72eb2f5a7784c571f17328ca3f
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-09-26 18:58:02 -0300

    Dagger hilt installed. Fixed error 'Kotlin compilation is running on a JDK, not JRE'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -27,27 +29,39 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
     }
 
     // View binding
     buildFeatures {
         viewBinding = true
     }
+    kotlin {
+        jvmToolchain(18)
+    }
 }
 
+//kapt {
+//    correctErrorTypes = true
+//}
+
 dependencies {
-    val navVersion = "2.7.1"
+    val navVersion = "2.7.3"
+    val dagger = "2.48"
 
     // NavComponent
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Dagger - hilt -> inyeccion de dependencias
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -55,4 +69,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
